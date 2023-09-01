c902c60ebea8eac3502c79908e20bf0142415402
ArsenyKornilcev <76811297+ArsenyKornilcev@users.noreply.github.com>
2023-09-01 22:01:03 +0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <learning-resource></learning-resource>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import LearningResource from '@/components/learning-resources/LearningResource'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+        LearningResource
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-gide',
+                    title: 'Official gide',
+                    description: 'The official Vue.js documentation',
+                    url: 'https://v3.ru.vuejs.org/ru'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to Google',
+                    url: 'https://google.com'
+                }
+            ]
+        };
+    },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
