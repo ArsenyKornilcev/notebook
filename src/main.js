2d68e7d66b586d2463a155bc661d209b7cc9d016
ArsenyKornilcev <76811297+ArsenyKornilcev@users.noreply.github.com>
2023-09-02 17:14:49 +0500

    new button component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import BaseCard from '@/components/UI/BaseCard'
+import BaseCard from "@/components/UI/BaseCard";
+import BaseButton from '@/components/UI/BaseButton'
 
 const app = createApp(App);
 
-app.component('BaseCard', BaseCard);
+app.component("BaseCard", BaseCard);
+app.component("BaseButton", BaseButton);
 
 app.mount("#app");
