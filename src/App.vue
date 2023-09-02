f89db027b92c57a31d5576cc4ccc228615ee449e
ArsenyKornilcev <76811297+ArsenyKornilcev@users.noreply.github.com>
2023-09-02 16:33:28 +0500

    new ul component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,34 @@
 <template>
-    <learning-resource></learning-resource>
+	<stored-resource :resources="storedResources"></stored-resource>
 </template>
 
 <script>
+	import StoredResource from './components/learning-resources/StoredResource.vue';
 
-import LearningResource from '@/components/learning-resources/LearningResource'
-
-export default {
-    name: 'App',
-    components: {
-        LearningResource
-    },
-    data() {
-        return {
-            storedResources: [
-                {
-                    id: 'official-gide',
-                    title: 'Official gide',
-                    description: 'The official Vue.js documentation',
-                    url: 'https://v3.ru.vuejs.org/ru'
-                },
-                {
-                    id: 'google',
-                    title: 'Google',
-                    description: 'Learn to Google',
-                    url: 'https://google.com'
-                }
-            ]
-        };
-    },
-};
+	export default {
+		name: "App",
+		components: {
+			StoredResource,
+		},
+		data() {
+			return {
+				storedResources: [
+					{
+						id: "official-gide",
+						title: "Official gide",
+						description: "The official Vue.js documentation",
+						url: "https://v3.ru.vuejs.org/ru",
+					},
+					{
+						id: "google",
+						title: "Google",
+						description: "Learn to Google",
+						url: "https://google.com",
+					},
+				],
+			};
+		},
+	};
 </script>
 
 <style></style>
---
+++ src/components/learning-resources/StoredResource.vue
@@ -0,0 +1,23 @@
+<template>
+	<ul>
+		<learning-resource
+			v-for="resource in resources"
+			:key="resource"
+			:title="resource.title"
+			:description="resource.description"
+			:link="resource.url">
+		</learning-resource>
+	</ul>
+</template>
+<script>
+	import LearningResource from "./LearningResource.vue";
+
+	export default {
+		name: "StoredResource",
+		components: {
+			LearningResource,
+		},
+		props: ["resources"],
+	};
+</script>
+<style></style>
